69c03c27e43395493b47601404194a93bef9accd
Guillaume Levrier <Guillaume-Levrier@users.noreply.github.com>
2024-09-06 09:28:57 +0200

    extracting config

---
+++ configuration.js
@@ -0,0 +1,52 @@
+const config = {
+  name: "Guillaume Levrier",
+  description: {
+    EN: {
+      short: `Guillaume Levrier is a political scientist working on biotechnology, currently a CNRS postdoc for the
+        <a href="https://cordis.europa.eu/project/id/951393" target="_blank">Nanobubbles ERC grant</a>. He is affiliated
+        to the <a href="http://umr-lisis.fr/membre/guillaume-levrier/" target="_blank">Laboratoire Interdisciplinaire
+          Sciences Innovations Sociétés (LISIS)</a>, and is a research associate to <a
+            href="https://www.sciencespo.fr/cevipof/en/researcher/guillaume-levrier.html"
+            target="_blank">CEVIPOF (Sciences Po)</a> and to the French National Library (BnF). `,
+      long: "",
+    },
+    FR: {
+      short: `Guillaume Levrier est un chercheur en sciences
+        politiques
+        spécialisé sur les biotechnologies, actuellement postdoc CNRS pour le <a
+          href="https://cordis.europa.eu/project/id/951393" target="_blank">projet
+          ERC Nanobubbles</a>. Il est affilié au <a
+            href="http://umr-lisis.fr/membre/guillaume-levrier/"
+            target="_blank">Laboratoire Interdisciplinaire
+          Sciences
+          Innovations Sociétés (LISIS)</a>, est associé au <a
+            href="https://www.sciencespo.fr/cevipof/fr/chercheur/guillaume-levrier.html"
+            target="_blank">CEVIPOF (Sciences Po)</a> et à la Bibliothèque nationale de
+        France (BnF).`,
+      long: "",
+    },
+  },
+  links: [
+    {
+      label: "twitter",
+      value: "@g_levrier",
+      url: "https://twitter.com/g_levrier",
+    },
+    {
+      label: "google scholar",
+      value: "profile",
+      url: "https://scholar.google.com/citations?user=IRiJFNMAAAAJ&hl=en",
+    },
+    {
+      label: "pubpeer",
+      value: "profile",
+      url: "https://pubpeer.com/search?q=guillaume+levrier",
+    },
+    {
+      label: "linked in",
+      value: "profile",
+      url: "https://www.linkedin.com/in/guillaume-levrier-phd-89156838/",
+    },
+  ],
+  email: { name: "guillaume.levrier", domain: "politique.science" },
+};
