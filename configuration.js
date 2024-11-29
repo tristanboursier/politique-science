456abc10757c4e74ade97cdd699de33595669657
Guillaume Levrier <Guillaume-Levrier@users.noreply.github.com>
2024-11-29 12:41:38 +0100

    Update configuration.js

--- configuration.js
+++ configuration.js
@@ -2,27 +2,27 @@ const config = {
   name: "Guillaume Levrier",
   description: {
     EN: {
-      short: `Guillaume Levrier is a political scientist working on biotechnology, currently a CNRS postdoc for the
-        <a href="https://cordis.europa.eu/project/id/951393" target="_blank">Nanobubbles ERC grant</a>. He is affiliated
-        to the <a href="http://umr-lisis.fr/membre/guillaume-levrier/" target="_blank">Laboratoire Interdisciplinaire
-          Sciences Innovations Sociétés (LISIS)</a>, and is a research associate to <a
+      short: `Guillaume Levrier is a political scientist working on biotechnology. He is a research associate to <a
             href="https://www.sciencespo.fr/cevipof/en/researcher/guillaume-levrier.html"
-            target="_blank">CEVIPOF (Sciences Po)</a> and to the French National Library (BnF). `,
+            target="_blank">CEVIPOF (Sciences Po)</a> and to the French National Library (BnF). He is a former CNRS postdoc for the
+        <a href="https://cordis.europa.eu/project/id/951393" target="_blank">Nanobubbles ERC grant</a> and was affiliated
+        to the <a href="http://umr-lisis.fr/membre/guillaume-levrier/" target="_blank">Laboratoire Interdisciplinaire
+          Sciences Innovations Sociétés (LISIS)</a>`,
       long: "",
     },
     FR: {
       short: `Guillaume Levrier est un chercheur en sciences
         politiques
-        spécialisé sur les biotechnologies, actuellement postdoc CNRS pour le <a
+        spécialisé sur les biotechnologies. Il est associé au <a
+            href="https://www.sciencespo.fr/cevipof/fr/chercheur/guillaume-levrier.html"
+            target="_blank">CEVIPOF (Sciences Po)</a> et à la Bibliothèque nationale de
+        France (BnF). Il était précédemment postdoc CNRS pour le <a
           href="https://cordis.europa.eu/project/id/951393" target="_blank">projet
-          ERC Nanobubbles</a>. Il est affilié au <a
+          ERC Nanobubbles</a> et affilié au <a
             href="http://umr-lisis.fr/membre/guillaume-levrier/"
             target="_blank">Laboratoire Interdisciplinaire
           Sciences
-          Innovations Sociétés (LISIS)</a>, est associé au <a
-            href="https://www.sciencespo.fr/cevipof/fr/chercheur/guillaume-levrier.html"
-            target="_blank">CEVIPOF (Sciences Po)</a> et à la Bibliothèque nationale de
-        France (BnF).`,
+          Innovations Sociétés (LISIS)</a>,.`,
       long: "",
     },
   },
