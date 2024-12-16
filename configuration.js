94a3e3b44c60cc19f34ade15e23fc33249a911f3
Guillaume Levrier <Guillaume-Levrier@users.noreply.github.com>
2024-12-16 10:47:36 +0100

    Update configuration.js

--- configuration.js
+++ configuration.js
@@ -43,6 +43,11 @@ const config = {
       url: "https://pubpeer.com/search?q=guillaume+levrier",
     },
     {
+      label: "orcid",
+      value: "3-4801-4770",
+      url: "https://orcid.org/0000-0003-4801-4770",
+    },
+    {
       label: "linked in",
       value: "profile",
       url: "https://www.linkedin.com/in/guillaume-levrier-phd-89156838/",
