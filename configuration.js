7e1f133b30a4f57c850a4f6fd9410b84f8c4df6b
tristanboursier <boursiertristan@icloud.com>
2025-03-21 11:36:59 -0400

    Update configuration.js

--- configuration.js
+++ configuration.js
@@ -5,19 +5,11 @@ const config = {
       short: `Tristan Boursier est docteur en science politique de l’Université de Montréal et de Sciences Po Paris (2023). Il est également diplômé de l'Université de Genève (2016). Sa thèse de doctorat porte sur les obstacles à la solidarité inclusive.
 Avant de rejoindre le CEVIPOF, il a été doctorant invité à l'Université d'Oxford sous la supervision de Cécile Laborde. Il a été également membre de différents réseaux de recherche comme le centre Jean Monnet de Montréal.
  
-Ses recherches remettent en question les conceptions traditionnelles de la solidarité politique, en se concentrant sur sa prétendue opposition à la diversité culturelle. Pour cela il interroge les fondements philosophiques de la controverse publique appelée "dilemme progressiste". Dans le cadre de son second axe de recherche, il aborde les questions liées au succès des extrêmes droites en ligne. Pour cela il étudie notamment le phénomène des influenceurs politiques et cartographiant leurs stratégies de diffusion et leurs affinités idéologiques.
- 
-Dans le cadre de ce second axe, il travaille comme chercheur postdoctorant auprès de Mélissa Blais à l'Université du Québec en Outaouais afin de comprendre les dynamiques transnationales au sein des extrêmes droites entre Amérique du Nord et Europe de l'Ouest. Dans le cadre de cette activité, il est également membre du Chantier sur l'antiféminisme et du Réseau québécois en études féministes (RéQEF).`,
+`,
       long: "",
     },
     FR: {
-      short: `Guillaume Levrier est un chercheur en sciences
-        politiques
-        spécialisé sur les biotechnologies. Il est associé au <a
-            href="https://www.sciencespo.fr/cevipof/fr/chercheur/guillaume-levrier.html"
-            target="_blank">CEVIPOF (Sciences Po)</a> et à la Bibliothèque nationale de
-        France (BnF). Il est aussi l'architecte de <a href="https://github.com/Guillaume-Levrier/PANDORAE"target="_blank">
-        PANDORÆ</a> et membre du <a href="https://www.ouvrirlascience.fr/college-codes-sources-et-logiciels/"target="_blank">comité pour la science ouverte</a>.`,
+      short: ``,
       long: "",
     },
   },
